ebd89f480bde88a5cae02f8d95313910f04f205f
Jaime Peña G <japegu2275@gmail.com>
2021-10-01 18:05:04 -0500

    Unificacion funciones

--- forms.js
+++ forms.js
@@ -1,10 +1,46 @@
-function checkCorreo (correo){
-    var validado = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
-    if (validado.test (correo)){
-        return true;
-    }
-    else {
-            return false;
+//Np se si dentro de las funciones vienen los id que se usaron en el primer Sprint//
+
+const 	expresion = {
+    documento: /^[0-9]{8,10}$/,
+    password: /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{8,}$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-aA-Z0-9-.]+$/
+}
+                                        
+function checkNumDocumento(valor,tipo){
+
+	if(tipo.value !== ''){
+		return false; 
+	} else {
+		if (expresion.documento.test(valor)) {
+			return true;
+		} else {
+			return false;
+        }
     }
 }
-modulo.export = checkCorreo;
+
+function checkContrasena(valor){
+
+    if(expresion.password.test(valor)){
+        alert(valor+" es valido ");
+        return true;        
+    }
+    else{
+        alert(valor+" NO es valido!");
+        return false;
+    }
+}
+
+function checkContrasena(valor){
+
+    if(expresion.password.test(valor)){
+        alert(valor+" es valido ");
+        return true;        
+    }
+    else{
+        alert(valor+" NO es valido!");
+        return false;
+    }
+}    
+
+modulo.export  = { checkNumDocumento, checkCorreo,checkContrasena}
